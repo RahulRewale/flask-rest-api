fc458a9489860aac280eb6fbba1be0d96cbee16f
Rahul Rewale <rahul.rewale95@gmail.com>
2022-01-12 22:15:23 +0530

    Added PostgreSQL environment variable and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +11,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'jeet'
 api = Api(app)
@@ -92,5 +93,5 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-	# db.init_app(app)
+	# db.init_app(app)	# fails on heroku since we run app.py through uwsgi, so need to move outside __main__
 	app.run(port=5000, debug=True)
